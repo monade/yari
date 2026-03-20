eca35e8ba52e94d584a1ef901bd44d9425164f95
Mattia Cecchini <mattia@monade.io>
2026-03-20 17:30:25 +0000

    improve raylib renderer

--- src/raycast/platform/raylib/renderer.c
+++ src/raycast/platform/raylib/renderer.c
@@ -1,17 +1,23 @@
 #include "../../renderer.h"
 #include <raylib.h>
 
+static Image frame_buffer;
+static Texture2D frame_texture;
+
 float get_frame_time() {
     return GetFrameTime();
 }
 
 void draw_rectangle(int x, int y, int width, int height, pixel_t color) {
-    DrawRectangle(x, y, width, height, GetColor(color));
+    ImageDrawRectangle(&frame_buffer, x, y, width, height, GetColor(color));
 }
 
 void renderer_init(int width, int height, const char *title, unsigned int target_fps) {
     InitWindow(width, height, title);
     SetTargetFPS(target_fps);
+    SetTraceLogLevel(LOG_WARNING);
+    frame_buffer = GenImageColor(width, height, BLACK);
+    frame_texture = LoadTextureFromImage(frame_buffer);
 }
 
 bool game_should_close() {
@@ -23,6 +29,8 @@ void begin_drawing() {
 }
 
 void end_drawing() {
+    UpdateTexture(frame_texture, frame_buffer.data);
+    DrawTexture(frame_texture, 0, 0, WHITE);
     EndDrawing();
 }
 
